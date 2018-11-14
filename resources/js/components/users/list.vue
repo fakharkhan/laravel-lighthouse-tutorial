cb790ff469c30e931a1300ef16d1ba875f889261
Fakhar  Khan <fakhar@softpyramid.com>
2018-11-14 17:14:08 +0500

    Added Light House and Users List Index

---
+++ resources/js/components/users/list.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="container">
+
+        <table class="table">
+            <tr>
+                <td>id</td>
+                <td>name</td>
+                <td>email</td>
+            </tr>
+            <tr v-for="user in users" :key="user.id">
+                <td>{{ user.id }}</td>
+                <td>{{ user.name }}</td>
+                <td>{{ user.email }}</td>
+            </tr>
+        </table>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    export default {
+        data() {
+            return {
+                users: [],
+                user : {}
+            }
+        },
+        mounted() {
+            //load users list
+            this.getUsers();
+
+        },
+
+        methods: {
+            async getUsers () {
+                try {
+                    const res = await axios.post(
+                            '/graphql', {
+                                query: '{ users { id name email} }'
+                            });
+                    this.users = res.data.data.users ;
+
+                } catch (e) {
+                    console.log('err', e)
+                }
+            }
+        }
+    }
+</script>
